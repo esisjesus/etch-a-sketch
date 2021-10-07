c3b486c49f88037eb57b11f90a88cd2dc774d57d
Jesus <jesisp17@gmail.com>
2021-10-06 22:44:13 -0300

    Color change interface working for pen and background

--- index.js
+++ index.js
@@ -1,14 +1,62 @@
-const grid = document.getElementById("container")
-
-function createGrid(a){
-    for(let i = 0; i < a*a; i++){
-        let gridElement= document.createElement('div')
-        gridElement.classList.add("cell")
-        grid.appendChild(gridElement)
+function initEditor(){
+    const globalConfig = {
+        penColor: "#000000",
+        bgColor: "#ffffff",
+        cellsAmount: 32,
     }
-    grid.style.gridTemplateColumns = `repeat(${a}, 1fr)`
-    grid.style.gridTemplateRows = `repeat(${a}, 1fr)`
+    // TOOLS' PANEL FUNCTIONS:
+    const penColorInput = document.getElementById("pen-color-panel")
+    const bgColorInput = document.getElementById("bg-color-panel")
+    penColorInput.addEventListener('input', changePenColor)
+    bgColorInput.addEventListener('input', paintBg)
+    function changePenColor(e){
+        globalConfig.penColor = e.target.value
+    }
+    function paintBg(e){
+        globalConfig.bgColor = e.target.value;
+        const grid = document.getElementById("container")
+        grid.style.backgroundColor = globalConfig.bgColor
+    }
+    
 
+
+    // GRID, CELLS AND STROKES FUNCTIONS:
+    function createGrid(a){
+        const grid = document.getElementById("container")
+        const panel = document.getElementById("panel")
+        for(let i = 0; i < a*a; i++){
+            let gridElement = document.createElement('div')
+            gridElement.classList.add("cell")
+            gridElement.addEventListener('mousedown', initStroke)
+            gridElement.addEventListener('mouseup', finishStroke)
+            grid.appendChild(gridElement)
+        }
+        grid.style.gridTemplateColumns = `repeat(${a}, 1fr)`
+        grid.style.gridTemplateRows = `repeat(${a}, 1fr)`
+        panel.addEventListener("mouseup", finishStroke)
+    }
+    
+    function initStroke(){
+        let cells = document.querySelectorAll(".cell")
+        this.style.backgroundColor = globalConfig.penColor
+        cells.forEach(cell => {
+            cell.addEventListener("mouseover", paintCell)
+        })
+    }
+    
+    function finishStroke(){
+        let cells = document.querySelectorAll(".cell")
+        cells.forEach(cell => {
+            cell.removeEventListener("mouseover", paintCell)
+        })
+    }    
+    
+    function paintCell(){
+        this.style.backgroundColor = globalConfig.penColor
+    }
+    createGrid(globalConfig.cellsAmount)
 }
 
-createGrid(100)
+
+initEditor()
+
